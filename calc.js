c2ffd7e2d0d08af02fd0e08844d7d08edeb449a1
Krishan Kumar <KrishanKuamr@email.kpu.ca>
2020-04-25 16:38:16 -0700

    calculator working

--- calc.js
+++ calc.js
@@ -1,8 +1,62 @@
-let buttons= document.getElementsByClassName('buttons');
-//buttons.forEach( val => {console.log(val)});
-console.log(buttons.length);
-for (var i = buttons.length - 1; i >= 0; i--) {
-	buttons[i].onclick= () => {display(i)};
+//variables
+let button= document.getElementsByClassName('buttons');
+let output= document.getElementsByClassName('output')[0];
+let calc= document.getElementsByClassName("calculation")[0];
+let clear= document.querySelector(".clear");
+let total= document.querySelector(".total");
+
+
+
+
+//functions
+let clearOutput= () => {output.value=""};
+let display= val => { console.log(`You pressed:${val.target.name}`)}
+let write= (e) => { 
+
+if(!( e.target.classList.contains("total") || e.target.classList.contains("clear")))
+{
+ let text= document.createTextNode(`${e.target.name}`);
+ calc.appendChild(text);
+}
 }
 
-let display= val => { console.log(`You pressed  ${val} button`)}
+//Creating mouse event funtions for all of the buttons
+for (var i = button.length - 1; i >= 0; i--) {
+ 	
+ 	button[i].onclick= (e) => {
+ 	 display(e);
+ 	 write(e);
+ 	 clearOutput();
+ 	};
+ }
+
+ //calculating the total
+total.addEventListener("click", function(e)
+{
+	let result;
+    try
+    {
+    	result =eval(calc.textContent);
+    	output.value= result;
+        calc.textContent="";
+    }
+    catch(e)
+    {
+    	console.log("Error:",e);
+        
+        output.style.color="red";
+        output.value ="INVALID OPERATION";
+
+    }
+	
+    
+});
+
+
+
+ //clearing the input
+clear.onclick= (e) => { 
+ calc.textContent= "";
+ clearOutput();
+ output.style.color="green";
+}
